8b65605dd7724e4c53b5121f2ae7da706838e689
Hyunmin Choi <chlgusals3@gmail.com>
2022-04-14 00:17:23 +0900

    a

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const myPage = require("./routes/myPage");
 const trip = require("./routes/trip");
 
 sequelize
-  .sync({ force: true })
+  .sync({ force: false })
   .then(() => {
     console.log("데이터베이스 연결 성공");
   })
